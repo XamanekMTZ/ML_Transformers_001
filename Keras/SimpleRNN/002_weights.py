38d2c7f8fbe222ab56ef4da820fe167b2ebc1ec3
Xamanek Mtz M - Blackberry <xamanekmtz@gmail.com>
2024-01-29 13:30:24 -0600

    Codigo ejemplo de una red neuronal convolucional simple Se agregaron código para calcular los weights manualmente y por función del modelo

--- Keras/SimpleRNN/002_weights.py
+++ Keras/SimpleRNN/002_weights.py
@@ -35,3 +35,37 @@ print('\n Weights hidden units\n wh = ', wh)
 print('\n Bias hidden units\n bh = ', bh)
 print('\n Weights output units\n wy = ', wy)
 print('\n Bias output units\n by = ', by)
+print('\n')
+
+x = np.array([1, 2, 3])
+
+# Reshape the input to the required sample_size x time_steps x features
+x_input = np.reshape( x, (1, 3, 1 ) )
+
+# Predict the output
+y_pred_model = demo_model.predict(x_input)
+
+
+# Compute the output manually
+m = 2
+# Hidden layer initialized to zeros
+h0 = np.zeros( m )
+
+# first hidden layer
+h1 = np.dot( x[0], wx ) + h0 + bh
+
+# second hidden layer
+h2 = np.dot( x[1], wx ) + np.dot( h1, wh ) + bh
+
+# third hidden layer
+h3 = np.dot( x[2], wx ) + np.dot( h2, wh ) + bh
+
+# output layer
+o3 = np.dot( h3, wy ) + by
+
+print('\n Hidden Layer 1\n h1 = ', h1)
+print('\n Hidden Layer 2\n h2 = ', h2)
+print('\n Hidden Layer 3\n h3 = ', h3)
+print('Prediction from network ', y_pred_model)
+print('Prediction from our computation ', o3)
+print('\n')
---
+++ Keras/SimpleRNN/003_sunspots_dataset_test.py
@@ -0,0 +1,130 @@
+import math
+
+from pandas import read_csv
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, SimpleRNN
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+# Simple Recurrent Neural Network - RNN
+def create_RNN( hidden_units, dense_units, input_shape, activation ):
+  model = Sequential()
+
+  model.add(
+    SimpleRNN( 
+      hidden_units, 
+      input_shape = input_shape, 
+      activation = activation[0] 
+    ) )
+  
+  model.add( 
+    Dense( 
+      units = dense_units, 
+      activation = activation[1] 
+      ) )
+  
+  model.compile( 
+    loss = 'mean_squared_error', 
+    optimizer = 'adam' 
+    )
+  
+  return model
+
+def get_train_test( url, split_percent = 0.8 ):
+  df = read_csv( url, usecols=[1], engine='python' )
+  data = np.array(df.values.astype( 'float32' ) )
+  scaler = MinMaxScaler(feature_range = ( 0, 1 ) )
+  data = scaler.fit_transform( data ).flatten()
+  n = len( data )
+  # Point for splitting data into train and test 
+  split = int( n * split_percent )
+  train_data = data[ range( split ) ]
+  test_data = data[ split: ]
+
+  return train_data, test_data, data
+
+# Prepare the input X and target Y
+def get_XY( dat, time_steps ):
+  # Indices of target array
+  Y_ind = np.arange( time_steps, len(dat), time_steps )
+  Y = dat[Y_ind]
+
+  # Prepare X 
+  rows_x = len(Y)
+  X = dat[range( time_steps * rows_x )]
+  X = np.reshape( X, ( rows_x, time_steps, 1 ) )
+  return X, Y
+
+# Print the Root Mean Square Error - RMSE
+def print_error( trainY, testY, train_predict, test_predict ):
+  # Error of predictions
+  train_rmse = math.sqrt( mean_squared_error( trainY, train_predict ) )
+  test_rmse = math.sqrt( mean_squared_error( testY, test_predict ) )
+  # Print RMSE
+  print( '\nTrain RMSE: %.3f RMSE' % ( train_rmse ) )
+  print( '\nTest RMSE: %.3f RMSE' % ( test_rmse ) )
+
+# Plot the result
+def plot_result( trainY, testY, train_predict, test_predict ):
+  actual = np.append( trainY, testY )
+  predictions = np.append( train_predict, test_predict )
+  rows = len( actual )
+  plt.figure( figsize = ( 15, 6), dpi = 80 )
+  plt.plot( range( rows ), actual )
+  plt.plot( range( rows ), predictions )
+  plt.axvline( x = len( trainY ), color = 'r' )
+  plt.legend( ['Actual', 'Predictions'] )
+  plt.xlabel( 'Observation number after given time steps' )
+  plt.ylabel( 'Sunspots scaled' )
+  plt.title( 'Actual and Predicted Values' )
+  # make the canvas interactive before showing the plot
+  #plt.show(block = True)
+  #plt.show( block = None )
+
+
+#url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-sunspots.csv"
+url = "/home/xamanek/PythonProjects/ML_Transformers_001/Datasets/monthly-sunspots.csv"
+#url = "../../Datasets/monthly-sunspots.csv"
+
+train_data, test_data, data = get_train_test( url )
+
+print( '\n train_data.shape = ', train_data.shape )
+print( '\n test_data.shape = ', test_data.shape )
+print( '\n data.shape = ', data.shape )
+print( '\n' )
+
+time_steps = 12
+trainX, trainY = get_XY( train_data, time_steps )
+testX, testY = get_XY( test_data, time_steps )
+
+print( '\n Train X = ', trainX )
+print( '\n Train Y = ', trainY )
+print( '\n Test X = ', testX )
+print( '\n Test Y = ', testY )
+print( '\n' )
+
+model = create_RNN( 
+  hidden_units = 3, 
+  dense_units = 1, 
+  input_shape = ( time_steps, 1 ), 
+  activation = ['tanh', 'tanh'] 
+  )
+
+model.fit( 
+  trainX, 
+  trainY, 
+  epochs = 20, 
+  batch_size = 1, 
+  verbose = 2 
+  )
+
+# Make predictions
+train_predict = model.predict( trainX )
+test_predict = model.predict( testX )
+
+#Mean square error 
+print_error( trainY, testY, train_predict, test_predict )
+
+plot_result( trainY, testY, train_predict, test_predict )
